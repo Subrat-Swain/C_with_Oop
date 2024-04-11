5139d8ca395ecc99c5a9511c8ece6d3a84fc1795
Subrat-Swain <subratswain19991@gmail.com>
2024-04-11 18:16:49 +0530

    Escape_sequence_comments

---
+++ 3_escape_seq.cpp
@@ -0,0 +1,26 @@
+// Program For Escape Sequence in C++
+#include<iostream>
+using namespace std;
+int main()
+    {
+        system("cls");
+        cout<<"This is a program of cpp";
+        cout<<"\n\\n This is a new line character";
+        cout<<"\n\\t This is a\ttab character";
+        cout<<"\n\"Double Quotes\"";
+        cout<<"\n\'Single Quote\'";
+        cout<<"\n\\r is a removing previous blank space      \rSubrat";
+        cout<<"\n\\a  \a\a\a\a\a\ais a alert charcter";
+        cout<<"\n\\b  is a backslash character \b ";
+        cout<<"\n*\n**\n***\n****\n*****";
+
+        // Single Line Comment 
+
+        /*
+            Multiline Comment 
+            // Comments Are Used To ignore Some Line of Code and
+                    also for providing some information about the code
+        */
+
+        return 0;
+    }
